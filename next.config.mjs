a7e73a10646e16eca5c629507d1c1dbfa26017fb
Mohammad Hussain Sarwari <sarwarihussain7@gmail.com>
2025-11-06 20:52:55 +0430

    test

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+    output: 'export',
+     images: {
+    unoptimized: true, // 👈 این خط مشکل را حل می‌کند
+  },
 };
 
 export default nextConfig;
